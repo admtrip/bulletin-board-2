99598e3f2e178976d2885e101778b263057a2d83
admtrip <atripat0@chicagobooth.edu>
2025-02-27 04:39:53 +0000

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,25 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "boards", :action => "index" })
+  devise_for :users
+
+  # Set root path to boards index
+  root "boards#index"
 
   # Routes for the Post resource:
-
-  # CREATE
-  post("/insert_post", { :controller => "posts", :action => "create" })
-          
-  # READ
-  get("/posts", { :controller => "posts", :action => "index" })
-  
-  get("/posts/:path_id", { :controller => "posts", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
-  
-  # DELETE
+  post("/insert_post", { :controller => "posts", :action => "create" })        
+  get("/posts", { :controller => "posts", :action => "index" })  
+  get("/posts/:path_id", { :controller => "posts", :action => "show" })  
+  post("/modify_post/:path_id", { :controller => "posts", :action => "update" })  
   get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
 
-  #------------------------------
-
   # Routes for the Board resource:
-
-  # CREATE
-  post("/insert_board", { :controller => "boards", :action => "create" })
-          
-  # READ
-  get("/boards", { :controller => "boards", :action => "index" })
-  
-  get("/boards/:path_id", { :controller => "boards", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_board/:path_id", { :controller => "boards", :action => "update" })
-  
-  # DELETE
+  post("/insert_board", { :controller => "boards", :action => "create" })          
+  get("/boards", { :controller => "boards", :action => "index" })  
+  get("/boards/:path_id", { :controller => "boards", :action => "show" })  
+  post("/modify_board/:path_id", { :controller => "boards", :action => "update" })  
   get("/delete_board/:path_id", { :controller => "boards", :action => "destroy" })
 
-  #------------------------------
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Devise authentication routes
+  devise_scope :user do
+    get("/users/sign_out", to: "devise/sessions#destroy", as: :signout)
+  end
 end
